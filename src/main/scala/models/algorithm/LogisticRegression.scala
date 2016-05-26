19d7b8b6899c1ec298f2eaaa0ca385a92338dd3c
Wilson Liao <droyuki@gmail.com>
2016-05-27 01:36:17 +0800

    LR is work!!

--- src/main/scala/models/algorithm/LogisticRegression.scala
+++ src/main/scala/models/algorithm/LogisticRegression.scala
@@ -9,11 +9,11 @@ import org.apache.spark.mllib.classification.{LogisticRegressionModel, LogisticR
   * Created by WeiChen on 2016/5/26.
   */
 class LogisticRegression extends LinearModel {
-  override def hyperParameterTuning(data: RDD[LabeledPoint],test:RDD[LabeledPoint], iteration: List[Int] = List(10, 100, 1000), threshold: List[Double]): List[(LogisticRegressionModel, (Double, Double), (Int, Double))] = {
+  override def hyperParameterTuning(data: RDD[LabeledPoint],test:RDD[LabeledPoint], iteration: List[Int] = List(10, 100, 1000), threshold: List[Double]): List[((Double, Double), LogisticRegressionModel, (Int, Double))] = {
     val model = new LogisticRegressionWithLBFGS().setNumClasses(2).run(data)
     val hyper = threshold.map { t =>
       val acc = accurate(model.clearThreshold().setThreshold(t), test)
-      (model, acc, (0,t))
+      (acc,model, (0,t))
     }
     hyper
   }
