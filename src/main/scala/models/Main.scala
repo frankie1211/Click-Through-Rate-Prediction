63f8b65487d88b0e30d6a3c0edc8b8dec33024b0
Wilson Liao <droyuki@gmail.com>
2016-05-27 00:31:46 +0800

    加入Cross validation.

--- src/main/scala/models/Main.scala
+++ src/main/scala/models/Main.scala
@@ -1,6 +1,7 @@
 package models
 
 import models.algorithm.LogisticRegression
+import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
@@ -12,11 +13,5 @@ object Main {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("test").setMaster("local").set("spark.default.parallelism", "1")
     val sc = new SparkContext(conf)
-    val data = RDD[LabeledPoint]
-    val test = RDD[LabeledPoint]
-    val hyperParameters = List(Map("threshold" -> 0.1), Map("threshold" -> 0.2))
-    val lr = new LogisticRegression
-    val best = lr.hyperParameterTuning(data, test, hyperParameters)
-
   }
 }
--- src/main/scala/models/algorithm/LogisticRegression.scala
+++ src/main/scala/models/algorithm/LogisticRegression.scala
@@ -1,21 +1,20 @@
 package models.algorithm
 
 import models.core.LinearModel
-import org.apache.spark.SparkContext
-import org.apache.spark.mllib.regression.{GeneralizedLinearModel, LabeledPoint}
+import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.rdd.RDD
-import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
+import org.apache.spark.mllib.classification.{LogisticRegressionModel, LogisticRegressionWithLBFGS}
 
 /**
   * Created by WeiChen on 2016/5/26.
   */
 class LogisticRegression extends LinearModel {
-  override def hyperParameterTuning(data: RDD[LabeledPoint],test:RDD[LabeledPoint], iteration: List[Integer] = List(10, 100, 1000), threshold: List[Double]): (GeneralizedLinearModel, (Double, Double)) = {
+  override def hyperParameterTuning(data: RDD[LabeledPoint],test:RDD[LabeledPoint], iteration: List[Int] = List(10, 100, 1000), threshold: List[Double]): List[(LogisticRegressionModel, (Double, Double), (Int, Double))] = {
     val model = new LogisticRegressionWithLBFGS().setNumClasses(2).run(data)
     val hyper = threshold.map { t =>
       val acc = accurate(model.clearThreshold().setThreshold(t), test)
-      (model, acc)
+      (model, acc, (0,t))
     }
-    hyper.maxBy(_._2._1)
+    hyper
   }
 }
