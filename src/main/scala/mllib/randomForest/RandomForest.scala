a615d20f4d2edfe48d11b071d04c3ccb6c7452b9
Ben Hu <benjamin658gae@gmail.com>
2016-05-26 15:15:50 +0800

    add random forest model class

---
+++ src/main/scala/mllib/randomForest/RandomForest.scala
@@ -0,0 +1,12 @@
+package mllib.randomForest
+
+import mllib.smallModel
+
+/**
+  * Created by benjamin658 on 2016/5/26.
+  */
+class RandomForest extends smallModel{
+  def train(): Unit ={
+
+  }
+}
