6b962a3f3968adc0526228a9e032073f60497d75
Ben Hu <benjamin658gae@gmail.com>
2016-05-27 00:56:22 +0800

    try random forest model

---
+++ src/main/scala/util/SparkInit.scala
@@ -0,0 +1,28 @@
+package util
+
+/**
+  * Created by benjamin658 on 2016/5/26.
+  */
+
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SparkInit {
+  val appName = "CTR"
+  val master = "local[*]"
+  val conf = new SparkConf()
+    .setAppName(appName)
+    .setMaster(master)
+    .set("spark.io.compression.codec", "org.apache.spark.io.LZ4CompressionCodec")
+  val sc = new SparkContext(conf)
+  val sqlContext = new SQLContext(sc)
+
+
+  def getSparkContext(): SparkContext = {
+    sc
+  }
+
+  def getSqlContext(): SQLContext = {
+    sqlContext
+  }
+}
